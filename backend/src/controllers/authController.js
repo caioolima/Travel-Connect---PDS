1a1e1e7697f8a9464c75df00d708d07e1dbe3a41
Caio Costa de Souza Lima <111019201+caioolima@users.noreply.github.com>
2024-01-12 15:36:44 -0300

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,18 +2,97 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
+const express = require('express');
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  firstName: String,
-  lastName: String,
-  phone: String,
-  email: String,
-  dob: Date,
-  gender: String,
-});
 
+// Função para registrar usuário
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, phone, password, confirmPassword, firstName, lastName, dob, gender } = req.body;
+
+    // Verifica se o usuário, email ou telefone já existem no banco
+    const existingUser = await User.findOne({ $or: [{ username }, { email }, { phone }] });
+
+    if (existingUser) {
+      const errors = {};
+
+      if (existingUser.username === username) {
+        errors.username = 'Nome de usuário já em uso.';
+      }
+
+      if (existingUser.email === email) {
+        errors.email = 'Endereço de email já em uso.';
+      }
+
+      if (existingUser.phone === phone) {
+        errors.phone = 'Número de telefone já em uso.';
+      }
+
+      return res.status(400).json({ success: false, errors, message: 'Falha ao registrar usuário.' });
+    }
+
+    // Verifica se a senha e a confirmação de senha coincidem
+    if (password !== confirmPassword) {
+      return res.status(400).json({ success: false, error: 'As senhas não coincidem.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username,
+      password: hashedPassword,
+      email,
+      phone,
+      firstName,
+      lastName,
+      dob,
+      gender,
+    });
+
+    await user.save();
+
+    // Alteração na resposta do backend
+    res.status(201).json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Erro interno ao registrar usuário.' });
+  }
+};
+
+
+// Função para fazer login
+exports.loginUser = async (req, res) => {
+  try {
+      const { email, password } = req.body;
+
+      // Verifica se o email e a senha foram fornecidos
+      if (!email || !password) {
+          return res.status(400).json({ error: 'Email e senha são campos obrigatórios.' });
+      }
+
+      // Verifica se o usuário existe com o email fornecido
+      const user = await User.findOne({ email });
+
+      if (!user) {
+          return res.status(401).json({ error: 'Credenciais inválidas. Email não encontrado.' });
+      }
+
+      // Verifica se a senha é correta
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+          return res.status(401).json({ error: 'Credenciais inválidas. Senha incorreta.' });
+      }
+
+      // Gera o token de autenticação
+      const token = jwt.sign({ userId: user._id }, 'seuSegredoDoToken', { expiresIn: '1h' });
+
+      res.status(200).json({ token, userId: user._id });
+  } catch (error) {
+      console.error('Erro durante o login:', error);
+      res.status(500).json({ error: 'Erro interno ao fazer login. Tente novamente mais tarde.' });
+  }
+};
 
 exports.checkFieldAvailability = async (req, res) => {
   try {
@@ -36,4 +115,4 @@ exports.checkFieldAvailability = async (req, res) => {
     console.error(`Erro ao verificar a disponibilidade de ${fieldName}:`, error);
     res.status(500).json({ available: false }); // Em caso de erro, considerar como não disponível
   }
-}; 
+};
